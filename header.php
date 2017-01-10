5273efb1baa0a2abe1c61f5daf1f77231b3fc8c8
Alex Horton <alexhortonmusic@gmail.com>
2017-01-10 15:29:03 -0600

    added most of content to markup

---
+++ header.php
@@ -0,0 +1,39 @@
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width">
+	<title><?php wp_title( '|', true, 'right' ); ?></title>
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<script src="https://use.fontawesome.com/022879ddbb.js"></script>
+	<!-- [if lt IE 9]>
+	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
+	<![endif] -->
+	<?php wp_head(); ?>
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" />
+
+</head>
+<body>
+  <div class="wrapper">
+    <nav>
+    	<div class='logo'>
+    		<img alt="Dwell Music City Logo and Village Real Estate Logo" src="<?php echo get_template_directory_uri(); ?>/assets/logo-broker-1.png" />
+    	</div>
+    	<div class='rightNav'>
+    		<div class='navContact'>
+    			<div>T</div>
+    			<div>T</div>
+    			<div>T</div>
+    		</div>
+    		<div class='navLinks'>
+    			<ul>
+    				<li>About</li>
+    				<li>Search Homes</li>
+    				<li>Communities</li>
+    				<li>Buyers and Sellers</li>
+    				<li>New Homes</li>
+    				<li>Blog</li>
+    				<li>Contact</li>
+    			</ul>
+    		</div>
+    	</div>
+    </nav>
